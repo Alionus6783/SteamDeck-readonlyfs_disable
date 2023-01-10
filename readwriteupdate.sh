110c52293db78c36e6930fd3202e9f6e17c41248
Alionus6783 <122236644+Alionus6783@users.noreply.github.com>
2023-01-10 17:00:48 -0500

    Updating script with timings
    Added sleep and waits in order to allow time for root authorization & process completion.

--- readwriteupdate.sh
+++ readwriteupdate.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/bash
 sudo steamos read-only disable
+sleep 10
 sudo pacman-key --init
+wait
 sudo pacman-key --populate
+wait
 sudo pacman-key --refresh-keys
+echo "File System Unlocked"
