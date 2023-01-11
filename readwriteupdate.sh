d9dc34a899d114f7c0b300b428afc5c5b42f0339
Ava <122236644+Alionus6783@users.noreply.github.com>
2023-01-10 21:38:09 -0500

    Replacing with local file
    This was a copy from memory, needs replacement due to errors

--- readwriteupdate.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-sudo steamos read-only disable
-sleep 10
-sudo pacman-key --init
-wait
-sudo pacman-key --populate
-wait
-sudo pacman-key --refresh-keys
-echo "File System Unlocked"
